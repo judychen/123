be9c880096e0ce7734f34ed4fd14f055673cfbcd
judychen <chenfenfen@foxmail.com>
2016-09-08 23:15:48 +0800

    Signed-off-by: judychen <chenfenfen@foxmail.com>

---
+++ js/myGrid.js
@@ -0,0 +1,98 @@
+var MG_SORT_NONE = -1;
+var MG_SORT_SELF = 99;
+var MG_SORT_ASCII = 0;
+var MG_SORT_NUM =1;
+
+var pageview_selected;
+
+var sort_by_938233;
+
+function pageview_ascii_sort_asc(a, b){
+    /*todo*/
+}
+
+function pageview_ascii_sort_des(a ,b){
+    /*todo*/
+}
+
+function pageview_number_sort_asc(a, b){
+    /*todo*/
+}
+
+function pageview_number_sort_des(a, b){
+    /*todo*/
+}
+
+function ip_sort_asc(a, b){
+    /*todo*/
+}
+
+function ip_sort(a, b){
+    /*todo*/
+}
+
+function inet_aton(ip){
+    /*todo*/
+}
+
+function interface_sort_dsc(a, b){
+    /*todo*/
+}
+
+function interface_sort_asc(a, b){
+    /*todo*/
+}
+
+function g_ethernet_sort(a, b){
+    /*todo*/
+}
+
+function ethernet_sort(a, b){
+    /*todo*/
+}
+
+function vlan_interface_sort(a, b){
+    /*todo*/
+}
+
+function wlan_bss_srot(a, b){
+    /*todo*/
+}
+
+
+
+
+function pageview_item(){
+    /*todo*/
+}
+
+function mG_changeClass(){
+    /*todo*/
+}
+
+function myGrid(){
+    /*todo*/
+}
+
+function pageview_oncbxclick(item){
+    /*todo*/
+}
+
+function pageview_ischecked(index){
+    /*todo*/
+}
+
+myGrid.prototype = {
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/validate.js
@@ -0,0 +1,152 @@
+function checkIP(value, advanced){
+    /*todo*/
+}
+
+function checkMask(mask){
+    /*todo*/
+}
+
+function isMacValid(mac){
+    /*todo*/
+}
+
+function isDomainNameValid(value){
+    /*todo*/
+}
+
+function checkVlanID(value){
+    /*todo*/
+}
+
+function check_wep_key_ascii(value){
+    /*todo*/
+}
+
+function check_psk_ascii(value){
+    /*todo*/
+}
+
+function checkVlan(value){
+    /*todo*/
+}
+
+function checkNumberRang(value, minValue, maxValue){
+    /*todo*/
+}
+
+function isHasChinese(value){
+    /*todo*/
+}
+
+function getStrLeng(str){
+    /*todo*/
+}
+
+function checkStringLen(value, minLen, maxLen){
+    /*todo*/
+}
+
+function isHexString(value){
+    /*todo*/
+}
+
+function isImageNameValid(value){
+    /*todo*/
+}
+
+function isConfigNameValid(value){
+    /*todo*/
+}
+
+function checkPassword(value){
+    /*todo*/
+}
+
+function checkUserName(value){
+    /*todo*/
+}
+
+function checkInputText(){
+    /*todo*/
+}
+
+function checkblank(value){
+    /*todo*/
+}
+
+function is_has_blank_or_question(value){
+    /*todo*/
+}
+
+function checkLetterAndNumber(value){
+    /*todo*/
+}
+
+function checkLetterAndNumberandUnderline(value){
+    /*todo*/
+}
+
+function isLeapYear(year){
+    /*todo*/
+}
+
+function checkSSID(value){
+    /*todo*/
+}
+
+function check_vlan_name_desc(value){
+    /*todo*/
+}
+
+function is_ip_multicast_address(value){
+    /*todo*/
+}
+
+function is_ip_multicast_address_reserved(value){
+    /*todo*/
+}
+
+function is_ip_loopback_address(value){
+    /*todo*/
+}
+
+function is_ip_unicast_address(ip, netmask){
+    /*todo*/
+}
+
+function is_equal_network_number(){
+    /*todo*/
+}
+
+function is_ip_and_gatewap_same_subnet(){
+    /*todo*/
+}
+
+function is_ip_reserved(){
+    /*todo*/
+}
+
+function checkPPPoEUserOrPwd(){
+    /*todo*/
+}
+
+function is_ip_netword(){
+    /*todo*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
